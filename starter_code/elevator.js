6aeb371a451b4860dd61c83fd8f526a8700c9953
Jana Stinn <jana-stinn@gmx.de>
2018-01-29 21:33:51 +0100

    first commit

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -3,17 +3,47 @@ class Elevator {
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+    this.direction  = 1;
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  start() {
+    this.interval = setInterval(function(){update()}, 1000);
+  }
+  stop() {
+    setTimeout(function(){
+      clearInterval(this.interval); 
+    }, 1);
+  }
+  update() {
+    console.log(`${this.floor}`);
+  }
+  _passengersEnter() {
+
+  }
+  _passengersLeave() { 
+    
+  }
+  floorUp() { 
+    if (this.floor >= 0 && this.floor < 10 && this.direction === 1) {
+      this.floor++;
+    }
+  }
+  floorDown() { 
+    if (this.floor <= 10 && this.floor > 0 && this.direction === -1) {
+      this.floor--;
+    }
+  }
+  call(person) {
+    this.requests.push(person);
+  }
+  log() { 
+    console.log('Direction ' + this.direction + '| Floor :' + this.floor);
+  }
 }
 
 module.exports = Elevator;
+
+let el = new Elevator();
+el.floorUp();
+el.log();
+
